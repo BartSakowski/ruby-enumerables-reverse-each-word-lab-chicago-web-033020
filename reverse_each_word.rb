4b33796a0beb64c563422d8ed22297535f8bfe5b
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-22 21:44:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(sentence1)
   new_string = sentence1.split(", ")
-  new_string.each{ |word| puts worrd.reverse}
+  new_string.each{ |word| puts word.reverse}
+
+  new_string
 
 
 

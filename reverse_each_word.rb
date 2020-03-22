3c8a25df1d23646b5bfecc19f5e1d2092f652968
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-22 23:08:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,3 @@
-require 'pry'
-
-
-def reverse_each_word(array) #takes in a string arguement
-  new_array = array.split(" ") #new array takes the string and splits the
-  reversed_array = new_array.each {|x| x.reverse!}
-  binding.pry
-
-  return reversed_array.join(" ")
-end
-
-def reverse_each_word(array)
-  new_array = array.split(" ")
-  new_array.collect {|x| x.reverse!}
-  new_array.join(" ")
-end
-
 def reverse_each_word(array)
   array.split(" ").collect {|x| x.reverse!}.join(" ")
 end
